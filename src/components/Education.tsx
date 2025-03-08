f9583c7131207063c21ee6aae41d52b83210f41d
Guillermo Trigo <guillermoatrigo@gmail.com>
2025-03-08 19:50:18 +0100

    Refactor Education component to use Accordion for better organization and readability

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -3,65 +3,88 @@ import python  from "../images/python.png"
 import jsHTMLcss  from "../images/jsHTMLcss.png"
 import Vuejs3  from "../images/Vuejs3.png"
 import Flutter_Dart  from "../images/Flutter&Dart.png"
-
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import { colors, Typography } from "@mui/material";
 
 const Education = ()=> {
     return(
         <div className={styles.educationContainer}>
+            <p>Education</p>
+                        <Accordion sx={{
+                            backgroundColor: "#f2eedd", // Use theme color
+                            "&:hover": {
+                            backgroundColor: "#bebfad", // Theme hover effect
+                            },
+                        }}>
+                            <AccordionSummary>
+                                <p>Highschool: CIC + IDEA</p>
+                                {/* sports, math MAP tests, */}
+                            </AccordionSummary>
+                        </Accordion>
+                        
+                        <Accordion defaultExpanded sx={{
+                            backgroundColor: "#f2eedd",
+                        }}>
+                            <AccordionSummary >
+                                <p>University: UE</p>
+                                {/* display grades */}
+                                {/* discribe projcts with links to porjects */}
+                            </AccordionSummary>
+                                <AccordionDetails>
+                                    <Accordion defaultExpanded sx={{
+                            backgroundColor: "#f2eedd", // Use theme color
+                        }}>
+                                        <AccordionSummary>Grades: </AccordionSummary>
+                                        <AccordionDetails>
+                                        <div className={styles.universityContainer}>
+                                            <div>
+                                                <p>Levels of Programming - 81.2%</p>
+                                            </div>
+                                            <div>
+                                                <p>Information Technology - 93.5%</p>
+                                            </div>
+                                            <div>
+                                                <p>Development Strategies - 85%</p>
+                                            </div>
+                                            <div>
+                                                <p>Management - 87%</p>
+                                            </div>
+                                            <div>
+                                                <p>Data Analytics - 83%</p>
+                                            </div>
+                                            <div>
+                                                <p>Front End Development - 93%</p>
+                                            </div>
+                                        </div>
+                                        </AccordionDetails>
+                                    </Accordion >
+                                        <Accordion defaultExpanded sx={{
+                            backgroundColor: "#f2eedd", 
+                        }}>
+                                            <AccordionSummary>Projects: </AccordionSummary>
+                                            <AccordionDetails>
+                                            <div className={styles.universityContainer}>
+                                                <div>
+                                                    <p>Hybrid Weather App</p>
+                                                </div>
+                                                <div>
+                                                    <p>Emotion Detection Deep Learning Model</p>
+                                                </div>
+                                                <div>
+                                                    <p>Iris Plant Classification</p>
+                                                </div>
+                                            </div>
+                                            </AccordionDetails>
+                                        </Accordion>
+                                </AccordionDetails>
+                        </Accordion>
+                        
             <div>
-                <h1>Education</h1>
-            </div>
-            <div>
-                {/* dropdown */}
+                
                 <div>
-                    <p>Highschool: CIC + IDEA</p>
-                    {/* sports, math MAP tests, */}
-                </div>
-                <div >
-                    <p>University: UE</p>
-                    {/* display grades */}
-                    {/* discribe projcts with links to porjects */}
-                    <div>
-                        <p>Grades: </p>
-                        <div className={styles.universityContainer}>
-                            <div>
-                                <p>Levels of Programming - 81.2%</p>
-                            </div>
-                            <div>
-                                <p>Information Technology - 93.5%</p>
-                            </div>
-                            <div>
-                                <p>Development Strategies - 85%</p>
-                            </div>
-                            <div>
-                                <p>Management - 87%</p>
-                            </div>
-                            <div>
-                                <p>Data Analytics - 83%</p>
-                            </div>
-                            <div>
-                                <p>Front End Development - 93%</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <p>Projects: </p>
-                        <div className={styles.universityContainer}>
-                            <div>
-                                <p>Hybrid Weather App</p>
-                            </div>
-                            <div>
-                                <p>Emotion Detection Deep Learning Model</p>
-                            </div>
-                            <div>
-                                <p>Iris Plant Classification</p>
-                            </div>
-                        </div>
-                    </div>
-                    
-                </div>
-                <div>
-                    <p>Online Course</p>
+                    <p>Online Courses</p>
                     {/* udemy */}
                     {/* harvard course */}
                     <div className={styles.onlineCoursesContainer}>
