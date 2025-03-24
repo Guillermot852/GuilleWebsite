49187c73e98bb8d27f65c04e0b53692f210779a3
Guillermo Trigo <guillermoatrigo@gmail.com>
2025-03-25 00:50:56 +0100

    Enhance Education component layout with Accordion for improved organization and readability; update styles for better responsiveness

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -7,38 +7,44 @@ import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import { colors, Typography } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const Education = ()=> {
     return(
         <div className={styles.educationContainer}>
             <p>Education</p>
-                        <Accordion sx={{
-                            backgroundColor: "#f2eedd", // Use theme color
-                            "&:hover": {
-                            backgroundColor: "#bebfad", // Theme hover effect
-                            },
-                        }}>
-                            <AccordionSummary>
-                                <p>Highschool: CIC + IDEA</p>
-                                {/* sports, math MAP tests, */}
-                            </AccordionSummary>
-                        </Accordion>
-                        
+            {/* Highschools */}
+                <Accordion sx={{
+                    backgroundColor: "#f2eedd",
+                    marginBottom: "10px",
+                }}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                        <p>Highschool: CIC + IDEA</p>
+                        {/* sports, math MAP tests, */}
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <p>sports, math map tests, etc..</p>
+                    </AccordionDetails>
+                </Accordion>
+            {/* University */}
+                <Accordion defaultExpanded sx={{
+                    backgroundColor: "#f2eedd",
+                    marginBottom: "10px",
+                }}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                        <p>University: UE</p>
+                        {/* display grades */}
+                        {/* discribe projcts with links to porjects */}
+                    </AccordionSummary>
+                    <AccordionDetails>
                         <Accordion defaultExpanded sx={{
-                            backgroundColor: "#f2eedd",
+                            backgroundColor: "#f2eedd", 
                         }}>
-                            <AccordionSummary >
-                                <p>University: UE</p>
-                                {/* display grades */}
-                                {/* discribe projcts with links to porjects */}
+                            <AccordionSummary expandIcon={<ExpandMoreIcon />}
+                                >Grades: 
                             </AccordionSummary>
-                                <AccordionDetails>
-                                    <Accordion defaultExpanded sx={{
-                            backgroundColor: "#f2eedd", // Use theme color
-                        }}>
-                                        <AccordionSummary>Grades: </AccordionSummary>
-                                        <AccordionDetails>
-                                        <div className={styles.universityContainer}>
+                            <AccordionDetails>
+                                <div className={styles.universityContainer}>
                                             <div>
                                                 <p>Levels of Programming - 81.2%</p>
                                             </div>
@@ -57,15 +63,17 @@ const Education = ()=> {
                                             <div>
                                                 <p>Front End Development - 93%</p>
                                             </div>
-                                        </div>
-                                        </AccordionDetails>
-                                    </Accordion >
-                                        <Accordion defaultExpanded sx={{
+                                </div>
+                            </AccordionDetails>
+                        </Accordion >
+                        {/* Projects */}
+                        <Accordion defaultExpanded sx={{
                             backgroundColor: "#f2eedd", 
                         }}>
-                                            <AccordionSummary>Projects: </AccordionSummary>
-                                            <AccordionDetails>
-                                            <div className={styles.universityContainer}>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon />}
+                            >Projects: </AccordionSummary>
+                                <AccordionDetails>
+                                    <div className={styles.universityContainer}>
                                                 <div>
                                                     <p>Hybrid Weather App</p>
                                                 </div>
@@ -75,42 +83,47 @@ const Education = ()=> {
                                                 <div>
                                                     <p>Iris Plant Classification</p>
                                                 </div>
-                                            </div>
-                                            </AccordionDetails>
-                                        </Accordion>
+                                    </div>
                                 </AccordionDetails>
-                        </Accordion>
-                        
-            <div>
-                
+                            </Accordion>
+                        </AccordionDetails>
+                    </Accordion>
                 <div>
-                    <p>Online Courses</p>
-                    {/* udemy */}
-                    {/* harvard course */}
-                    <div className={styles.onlineCoursesContainer}>
-                        <div>
-                            <img src={python} alt="Photo of Guillermo Trigo" />
-                            <p>The complete Python Developer;</p>
-                            <p>Andri Neagoie</p>
+                    <Accordion sx={{
+                            backgroundColor: "#f2eedd", 
+                            marginBottom: "10px",
+                        }}>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                            <p>Online Courses</p>
+                            {/* udemy */}
+                            {/* harvard course */}
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <div className={styles.onlineCoursesContainer} >
+                            <div style={{boxShadow: "none"}}>
+                                <img src={python} alt="Photo of Guillermo Trigo" />
+                                <p>The complete Python Developer;</p>
+                                <p>Andri Neagoie</p>
+                            </div>
+                            <div>
+                                <img src={jsHTMLcss} alt="Photo of Guillermo Trigo" />
+                                <p>Javascript, HTML and CSS;</p>
+                                <p>OAK Academy, OAK Academy Team</p>
+                            </div>
+                            <div>
+                                <img src={Vuejs3} alt="Photo of Guillermo Trigo" />
+                                <p>Vue js 3;</p>
+                                <p>Ignacio bluuweb</p>
+                            </div>
+                            <div>
+                                <img src={Flutter_Dart} alt="Photo of Guillermo Trigo" />
+                                <p>Flutter & Dart;</p>
+                                <p>Academind by Maximilian Schwarzmüller</p>
+                            </div>
                         </div>
-                        <div>
-                            <img src={jsHTMLcss} alt="Photo of Guillermo Trigo" />
-                            <p>Javascript, HTML and CSS;</p>
-                            <p>OAK Academy, OAK Academy Team</p>
-                        </div>
-                        <div>
-                            <img src={Vuejs3} alt="Photo of Guillermo Trigo" />
-                            <p>Vue js 3;</p>
-                            <p>Ignacio bluuweb</p>
-                        </div>
-                        <div>
-                            <img src={Flutter_Dart} alt="Photo of Guillermo Trigo" />
-                            <p>Flutter & Dart;</p>
-                            <p>Academind by Maximilian Schwarzmüller</p>
-                        </div>
-                    </div>
+                        </AccordionDetails>
+                    </Accordion>
                 </div>
-            </div>
         </div>
     )
 }
