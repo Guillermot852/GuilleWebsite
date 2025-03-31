014fc38c68f2cf12efc8b0410b93421d2512fd59
Guillermo Trigo <guillermoatrigo@gmail.com>
2025-03-31 22:38:05 +0200

    experience added

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,10 +2,45 @@ import styles from "../screenStyles/HomeScreen.module.css";
 
 const Experience = ()=> {
     return(
-        <div className={styles.experienceContainer}>
-            <div>
-                <div></div>
-            </div>
+        <div className={styles.educationContainer}>
+                <div style={{display: 'flex', flexDirection: 'row'}}>
+                    {/* logo + line*/}
+                    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px'}}>
+                        <img width='50px' src="/images/TU-Berlin-Logo.svg.png" alt="TU Logo" />
+                        {/* vertical line: */}
+                        <div style={{ width: "2px", backgroundColor: "#ACACAC", height: "100%",}}></div>
+                    </div>
+                    
+                    {/* information */}
+                    <div style={{display: 'flex', flexDirection: 'column', marginLeft: '8px', gap: '10px'}} >
+                        {/* Company/general info */}
+                        <div >
+                                {/* Company Name */}
+                                <h4 style={{margin: 0}}>Technische Universitet Berlin</h4>
+                                {/* job type internship/fulltime/partime etc...*/}
+                                <p style={{margin: 0}}>Internship</p>
+                                {/* city * hybrid/ remote/ x*/}
+                                <p style={{margin: 0}}>Berlin, Germany * Hybrid</p>
+                        </div>
+
+                        {/* Position information */}
+                        <div>
+                                <h4 style={{margin: 0}}>Research Team Lead (UI)</h4>
+                                <p style={{margin: 0, marginBottom: '10px'}}>January 2025 - Present * 3 mos</p>
+                                <div style={{marginLeft: "14px"}}>
+                                    <li>
+                                        <p style={{margin: 0}}>Leading the development and design of the XDB-UI project</p>
+                                    </li>
+                                    <li>
+                                        <p style={{margin: 0}}>Managing team workflows and prioritizing tasks</p>
+                                    </li>
+                                    <li>
+                                        <p style={{margin: 0}}>Coordinating with cross-functional teams</p>
+                                    </li>
+                                </div>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,10 +2,12 @@ import Details from "../components/Details";
 import Education from "../components/Education";
 import styles from "../screenStyles/HomeScreen.module.css";
 import HardSkills from "../components/HardSkills"
+import Experience from "../components/Experience";
 const HomeScreen = ()=> {
     return(
         <div className={styles.mainContainer}>
             <Details/>
+            <Experience/>
             <HardSkills/>
             <Education/>
         </div>
