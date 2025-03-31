ec1a96f8b09a92e959654a5b388cd674f0e07fac
Guillermo Trigo <guillermoatrigo@gmail.com>
2025-03-31 22:38:04 +0200

    Experience added

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,14 +1,12 @@
 import styles from "../screenStyles/HomeScreen.module.css";
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-const Education = ()=> {
+const Experience = ()=> {
     return(
-        <div className={styles.educationContainer}>
-            
+        <div className={styles.experienceContainer}>
+            <div>
+                <div></div>
+            </div>
         </div>
     )
 }
-export default Education
+export default Experience
