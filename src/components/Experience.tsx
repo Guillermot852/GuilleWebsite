4332637d9d7b4b7eab2dfce0266f7896b0f83155
Guillermo Trigo <guillermoatrigo@gmail.com>
2025-04-01 09:12:27 +0200

    enhanced Experience component with additional roles and improved styling

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -11,9 +11,9 @@ const Experience = ()=> {
                         <div className={styles.verticalLine}></div>
                     </div>
                     
-                    {/* information */}
+                    {/* TU */}
                     <div className={styles.informationContainer}>
-                        {/* Company/general info */}
+                        {/* TU Dima Labs */}
                         <div >
                                 {/* Company Name */}
                                 <h4>Technische Universitet Berlin</h4>
@@ -22,11 +22,11 @@ const Experience = ()=> {
                                 {/* city * hybrid/ remote/ x*/}
                                 <p>Berlin, Germany * Hybrid</p>
                         </div>
-
-                        {/* Position information */}
-                        <div>
+                        
+                        {/* Team Lead */}
+                        <div style={{marginBottom: '10px'}}>
                                 <h4>Research Team Lead (UI)</h4>
-                                <p style={{marginBottom: '10px'}}>January 2025 - Present * 3 mos</p>
+                                <p style={{marginBottom: '10px'}}>January 2025 - Present * Ongoing</p>
                                 <div style={{marginLeft: "14px"}}>
                                     <li>
                                         <p>Leading the development and design of the XDB-UI project</p>
@@ -39,8 +39,99 @@ const Experience = ()=> {
                                     </li>
                                 </div>
                         </div>
+                        {/* Research Assistant */}
+                        <div>
+                                <h4>Research Assistant (UI)</h4>
+                                <p style={{marginBottom: '10px'}}>March 2024 - January 2025 * 11 mos</p>
+                                <div style={{marginLeft: "14px"}}>
+                                    <li>
+                                        <p>Building the front end of a Cross-Database Query Processing System using React and
+                                        TypeScript and MUI.</p>
+                                    </li>
+                                    <li>
+                                        <p>Constructed a mock-back-end server to provide API endpoints and perform query calculations, 
+                                            ensuring efficient data handling for front-end integration.</p>
+                                    </li>
+                                    <li>
+                                        <p>Assisted the back end team with code reviews and actively participated in team meetings.</p>
+                                    </li>
+                                </div>
+                        </div>
                     </div>
                 </div>
+
+                {/* Lacoste */}
+                <div className={styles.rowContainer}>
+                    {/* logo + line*/}
+                    <div className={styles.logoLine}>
+                        <img width='50px' src="/images/lacoste-logo.png" alt="Lacoste Logo" />
+                        {/* vertical line: */}
+                        <div className={styles.verticalLine}></div>
+                    </div>
+                    
+                    {/* Lacoste */}
+                    <div className={styles.informationContainer}>
+                        <div >
+                                <h4>Lacoste</h4>
+                                <p>Part Time</p>
+                                <p>Berlin, Germany</p>
+                        </div>
+                        
+                        <div style={{marginBottom: '10px'}}>
+                                <h4>Sales Assistant</h4>
+                                <p style={{marginBottom: '10px'}}>March 2024 - January 2025 * 11 mos</p>
+                                <div style={{marginLeft: "14px"}}>
+                                    <li>
+                                        <p>Focused on improving sales skills and customer relations</p>
+                                    </li>
+                                    <li>
+                                        <p>Implemented Organizational designs to decrease employee onboarding time and increase other KPIs</p>
+                                    </li>
+                                    <li>
+                                        <p>Increased sales by improving customer interaction</p>
+                                    </li>
+                                    <li>
+                                        <p>Created a visually welcoming store with innovative designs</p>
+                                    </li>
+                                    <li>
+                                        <p>Educated employees to provide customers with 
+                                        inspiring information about clothes or general Lacoste history</p>
+                                    </li>
+                                </div>
+                        </div>
+                    </div>
+
+                
+                </div>
+                {/* Pret A Manger */}
+                <div className={styles.rowContainer}>
+                    <div className={styles.logoLine}>
+                        <img width='50px' src="/images/pret-a-manger-logo.png" alt="Pret A Manger Logo" />
+                        <div className={styles.verticalLine}></div>
+                    </div>
+                    
+                    <div className={styles.informationContainer}>
+                        <div >
+                                <h4>Pret A Manger</h4>
+                                <p>Part Time</p>
+                                <p>Berlin, Germany</p>
+                        </div>
+                        
+                        <div style={{marginBottom: '10px'}}>
+                                <h4>Shift Leader</h4>
+                                <p style={{marginBottom: '10px'}}>Oct 2023 - March 2024 * 6 mos</p>
+                                <div style={{marginLeft: "14px"}}>
+                                    <li>
+                                        <p>Managed team schedules, reduced onboarding time by 20%, demonstrating leadership and organizational skills</p>
+                                    </li>
+                                    <li>
+                                        <p>Created a cohesive and efficient working environment for the team, resulting in improved performance and morale</p>
+                                    </li>
+                                    
+                                </div>
+                        </div>
+                    </div>
+                    </div>
         </div>
     )
 }
